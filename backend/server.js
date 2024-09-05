2cb433d33b53a421e32801978f8fb46d895d141e
AnasAhmed <hafeezroll69@gmail.com>
2024-09-06 01:45:16 +0500

    vercel.json3

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,12 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
+import { fileURLToPath } from 'url';
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
-const __dirname = path.resolve();
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
@@ -23,11 +23,16 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "./dist")));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "dist")));
+
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "./dist/index.html"));
+    res.sendFile(path.join(__dirname, "./dist/index.html"));
 });
+
 
 server.listen(PORT, () => {
 	connectToMongoDB();

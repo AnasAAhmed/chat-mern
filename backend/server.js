e67d149f5e6ee9c9a1248da86fd77c2a03120d8e
AnasAhmed <hafeezroll69@gmail.com>
2024-09-06 01:54:17 +0500

    vercel.json4

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, "dist")));
 
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./dist/index.html"));
+    res.sendFile(path.resolve(__dirname, "./dist/index.html"));
 });
 
 

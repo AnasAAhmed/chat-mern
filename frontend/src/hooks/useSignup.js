70ea7ab5b2083b910f88d35616a89731f3a1f189
AnasAhmed <hafeezroll69@gmail.com>
2024-09-07 18:47:52 +0500

    sign-up url fix

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -12,7 +12,7 @@ const useSignup = () => {
 
 		setLoading(true);
 		try {
-			const res = await fetch(`${BACK_URL}/api/auth/signup`, {
+			const res = await fetch(`${import.meta.env.VITE_BACK_URL}/api/auth/signup`, {
 				method: "POST",
 				headers: { "Content-Type": "application/json" },
 				body: JSON.stringify({ fullName, username, password, confirmPassword, gender }),
